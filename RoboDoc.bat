8321b6bc88498869a841cc3d35869f77c52f923c
Roman Horváth <raubirius@gmail.com>
2024-03-09 05:02:43 +0100

    Významný upgrade metódy Svet.dialóg…
    (Ešte sa bude podrobnejšie testovať. Je možné, že do pár dní pribudne ďalší update, ale dúfam, že nie.)

--- RoboDoc.bat
+++ RoboDoc.bat
@@ -1,6 +1,26 @@
 @echo off
-chcp 1250
-java -Dfile.encoding=UTF8 -jar "c:\_jEdit\launcher.jar" "c:\Users\Public\Documents\launcher-file.txt" "c:\Program Files\Java\jdk1.8.0_341\bin"
+chcp 65001>nul
+
+set rodocPath=c:\_Sync\Vyucba\Materialy\@Robot\RoboDoc
+set rodocName=RoboDoc
+set javaPath=c:\Program Files\Java\jdk-1.8\bin
+
+call c:\_jEdit\findJava.bat
+echo javaPath: %javaPath%
+
+if not exist "%rodocPath%\%rodocName%.class" (
+	echo Compiling %rodocName%.
+	"%javaPath%\java" -Dfile.encoding=UTF8 -jar "c:\_jEdit\compiler.jar" "%rodocPath%\compile-%rodocName%.txt" "%javaPath%" -q
+)
+
+if not exist "%rodocPath%\%rodocName%.class" (
+	echo Failed…
+	pause>nul
+	goto :eof
+)
+
+"%javaPath%\java" -Dfile.encoding=UTF8 -jar "c:\_jEdit\launcher.jar" "%rodocPath%\launch-%rodoc%.txt" "%javaPath%" -q
+
 echo.
 echo Finished.
 echo.
